775fc434ff4083283210497cb5d1c28a577c4934
Fermin Tene <sefertel@outlook.com>
2015-06-18 08:17:20 -0500

    proyecto iniciado con template admin

---
+++ resources/views/templates/admin_tpl.blade.php
@@ -0,0 +1,215 @@
+﻿<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>e@sybrok</title>
+        <!-- BOOTSTRAP STYLES-->
+        <link href="assets/css/bootstrap.css" rel="stylesheet" />
+        <!-- FONTAWESOME ICONS STYLES-->
+        <link href="assets/css/font-awesome.css" rel="stylesheet" />
+        <!--CUSTOM STYLES-->
+        <link href="assets/css/style.css" rel="stylesheet" />
+        <!-- HTML5 Shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <div id="wrapper">
+            <nav class="navbar navbar-default navbar-cls-top " role="navigation" style="margin-bottom: 0">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a  class="navbar-brand" href="index.html">easybrok</a>
+                </div>
+                <div class="notifications-wrapper">
+                    <ul class="nav">
+                        <li class="dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown" href="#" aria-expanded="false">
+                                <i class="fa fa-tasks fa-fw"></i>  
+                                <i class="fa fa-caret-down"></i>
+                            </a>
+                            <ul class="dropdown-menu dropdown-tasks">
+                                <li>
+                                    <a href="#">
+                                        <div>
+                                            <p>
+                                                <strong>Task 1</strong>
+                                                <span class="pull-right text-muted">60% Complete</span>
+                                            </p>
+                                            <div class="progress progress-striped active">
+                                                <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%">
+                                                    <span class="sr-only">60% Complete (danger)</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li class="divider"></li>
+                                <li>
+                                    <a href="#">
+                                        <div>
+                                            <p>
+                                                <strong>Task 2</strong>
+                                                <span class="pull-right text-muted">30% Complete</span>
+                                            </p>
+                                            <div class="progress progress-striped active">
+                                                <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100" style="width: 30%">
+                                                    <span class="sr-only">30% Complete</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li class="divider"></li>
+                                <li>
+                                    <a href="#">
+                                        <div>
+                                            <p>
+                                                <strong>Task 3</strong>
+                                                <span class="pull-right text-muted">80% Complete</span>
+                                            </p>
+                                            <div class="progress progress-striped active">
+                                                <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
+                                                    <span class="sr-only">80% Complete (warning)</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li class="divider"></li>
+                                <li>
+                                    <a href="#">
+                                        <div>
+                                            <p>
+                                                <strong>Task 4</strong>
+                                                <span class="pull-right text-muted">90% Complete</span>
+                                            </p>
+                                            <div class="progress progress-striped active">
+                                                <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width: 90%">
+                                                    <span class="sr-only">90% Complete (success)</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li class="divider"></li>
+                                <li>
+                                    <a class="text-center" href="#">
+                                        <strong>See Tasks List + </strong>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                                <i class="fa fa-user-plus"></i>  <i class="fa fa-caret-down"></i>
+                            </a>
+                            <ul class="dropdown-menu dropdown-user">
+                                <li><a href="#"><i class="fa fa-user-plus"></i> My Profile</a>
+                                </li>
+                                <li class="divider"></li>
+                                <li><a href="#"><i class="fa fa-sign-out"></i> Logout</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <!-- /. NAV TOP  -->
+            <nav  class="navbar-default navbar-side" role="navigation">
+                <div class="sidebar-collapse">
+                    <ul class="nav" id="main-menu">
+                        <li>
+                            <div class="user-img-div">
+                                <img src="assets/img/user.jpg" class="img-circle" />
+                            </div>
+                        </li>
+                        <li><a  href="#"> <strong> Citytech  </strong></a></li>
+                        <!--ITEMS DEL MENU -->
+                        <li><a class="active-menu"  href="inicio"><i class="fa fa-dashboard "></i>Inicio</a></li>
+                        <li><a href="clientes"><i class="fa fa-user"></i>Clientes</a></li>
+                        <li><a href="cotizaciones"><i class="fa fa-cubes"></i>Cotizaciones</a></li>
+                        <li>
+                            <a href="#">
+                                <i class="fa fa-cogs"></i>Admin<span class="fa arrow"></span>
+                            </a>
+                            <ul class="nav nav-second-level">
+                                <li><a href="#"><i class="fa fa-users"></i>Usuarios</a></li>
+                                <li>
+                                    <a href="#">Parametrización<span class="fa arrow"></span></a>
+                                    <ul class="nav nav-third-level">
+                                        <li><a href="#">País</a></li>
+                                        <li><a href="#">Provincias</a></li>
+                                        <li><a href="#">Ciudades</a></li>
+                                                <li>
+
+                                                   <a href="#">Cargos<span class="fa arrow"></span></a>
+                                                    <ul class="nav nav-third-level">
+                                                        <li><a href="create"> 1. Ingreso</a></li>
+                                                        <li><a href="listar"> 2. Listar</a></li>
+                                                        <li><a href="cargos/show"> 3. Modificar</a></li>
+                                                        <li><a href="#"> 4. Eliminar</a></li>
+                                                    </ul>
+
+                                                   <a href="cargos/list">Cargos<span class="fa arrow"></span></a>
+
+                                                </li>
+
+                                            </ul>
+                                         <ul class="nav nav-second-level">
+                                                <li>
+                                                   <a href="#">Concesionario<span class="fa arrow"></span></a>
+                                                    <ul class="nav nav-third-level">
+                                                        <li><a href="concesionario/create"> 1. Ingreso</a></li>
+                                                        <li><a href="concesionario/listar"> 2. Listar</a></li>
+                                                        <li><a href="concesionario/show"> 3. Modificar</a></li>
+                                                        
+                                                        
+                                                    </ul>
+                                                </li>
+                                            </ul>
+ </li>
+
+                                      
+                                      </li>
+
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <!-- /. SIDEBAR MENU (navbar-side) CONTAINNER -->
+            <!-- CONTAINNER -->
+            <div id="page-wrapper" class="page-wrapper-cls"> <!-- PAGE WRAPPER  -->
+                <div id="page-inner"> <!-- PAGE INNER  -->
+                    @yield('content')
+                </div> <!-- /. PAGE INNER  -->
+            </div> <!-- /. PAGE WRAPPER  -->
+        </div>
+        <!-- /. WRAPPER  -->
+        <footer > &copy; 2015 <a href="http://www.citytech.ec/" target="_blank">Citytech</a>
+        </footer>
+        <!-- /. FOOTER  -->
+        <!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
+        <!-- JQUERY SCRIPTS -->
+        <script src="assets/js/jquery-1.11.1.js"></script>
+        <!-- BOOTSTRAP SCRIPTS -->
+        <script src="assets/js/bootstrap.js"></script>
+        <!-- METISMENU SCRIPTS -->
+        <script src="assets/js/jquery.metisMenu.js"></script>
+        <!-- CUSTOM SCRIPTS -->
+        <script src="assets/js/custom.js"></script>
+
+
+    </body>
+</html>
